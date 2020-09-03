c368cf3d8a01fcbf7fada36df84efdacfbf27ab4
exploitpnk <exploitpnk@gmail.com>
2020-09-03 01:28:02 -0500

    [Update] Comments and pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,13 @@
 from flask import Flask
+""" starts a Flask web application """
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def index():
+def hello_hbtn():
+    """ Show Hello HBNB when / is called """
     return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
